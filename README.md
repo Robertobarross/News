3637b8a8933b2cb10ffe5af0b670bc3195c3bb92
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-11-19 20:52:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# News
+ Criando página de jornal para o Curso de Desenvolvimento Web Intermediário. HTML, CSS, PHP, MySQL.
---
+++ pages/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/projetos-php/news/components/style.css">
+    <title>News</title>
+</head>
+<body>
+
+
+    <div id="topo">
+        Topo
+    </div>
+
+    <menu>
+        menu
+    </menu>
+
+    <nav>
+        <hr>
+        Nav 1
+    </nav>
+
+    <div id="nav-inferior">
+        nav 2
+    </div>
+
+    <main>
+        Centro
+    </main>
+
+    <footer>
+        Rodapé
+    </footer>
+
+
+</body>
+</html>
