52c16a9f95c20d42922b8c82ca5fbe48296e1d26
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-11-20 00:04:16 -0300

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -11,20 +11,48 @@
 
 
     <div id="topo">
-        Topo
+        <img src="/projetos-php/news/components/imgs/globo-terrestre.jpg" class="clobo-terrestre" alt="Globo terrestre">
+        <h1>NEWS</h1>
+
+        <a href="#" class="links-topo">E-mail</a>
     </div>
 
     <menu>
-        menu
+        <button title="MENU" type="button" onclick="if(document.getElementById('menu') .style.display=='none') {document.getElementById('menu') .style.display=''}
+        else{document.getElementById('menu') .style.display='none'}" class="mostrar-ocultar">
+        <div> 
+            <img src="/projetos-php/news/components/imgs/img-menu.png" alt="Menu" class="botao-menu">
+        </div>
+        </button>
+        <div id="menu" style="display:none">
+            <li><a href="#" class="links-li">Mundo</a></li>
+            <li><a href="#" class="links-li">Economia</a></li>
+            <li><a href="#" class="links-li">Sports</a></li>
+            <li><a href="#" class="links-li">Mercado</a></li> 
+            <li><a href="#" class="links-li">Varejo</a></li>        
+            <li><a href="#" class="links-li">Turismo</a></li>        
+            <li><a href="#" class="links-li">Atacado</a></li>               
+        </div>
+        </div>
+
+        <h2>NEWS</h2>
+        <h3>A NOTÍCIA EM PRIMEIRO LUGAR</h3>
     </menu>
 
     <nav>
         <hr>
-        Nav 1
+        <br>
+        <a href="#" class="links-nav">Últimas Notícias</a>
+        <a href="#" class="links-nav">Mundo</a>
+        <a href="#" class="links-nav">Cotidiano</a>
+        <a href="#" class="links-nav">Poder</a>
+        <a href="#" class="links-nav">Cultura</a>
+        <a href="#" class="links-nav">Sports</a>
+        </br>
     </nav>
 
     <div id="nav-inferior">
-        nav 2
+        <a href="#" class="link-nav-inferior">Assine o News e tenha sempre as melhores notícias em primeira mão!</a>
     </div>
 
     <main>
