df0b9de076c036562fa6c153ef667fbdf3124c85
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-11-26 23:39:30 -0300

    Create connect-db.php

---
+++ components/connect-db.php
@@ -0,0 +1,13 @@
+<?php
+
+// Conexão com banco de dados
+// Variáveis
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "db_news";
+
+// Link da conexão
+$link = mysqli_connect($host, $user, $pass, $db);
+
+?>
