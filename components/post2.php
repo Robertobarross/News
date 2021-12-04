8a3955368e64b628c9c6609b78d4fc4f59c28fb7
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-12-03 23:44:01 -0300

    Update post2.php

---
+++ components/post2.php
@@ -0,0 +1,25 @@
+<?php
+
+// Arquivo responsável por inserir as informações no bd
+include 'connect-db.php'; // Link da conexão
+date_default_timezone_set('America/Sao_Paulo');// Define a data
+session_start();// inicia ou recupera a sessão
+
+
+// Variáveis que serão preenchidas no bd //
+@$tema = $_POST['tema']??'';
+@$imagem = $_FILES['imagem']['type']??'';
+@$imagem = $_FILES['imagem']['name']??'';
+@$titulo = $_POST['titulo']??'';
+@$relevantes = $_POST['relevantes']??'';
+@$noticia = $_POST['noticia']??'';
+@$dt = $_POST['dt']??'';
+@$hr = $_POST['hr']??'';
+@$usuario = $_POST['usuario']??'';
+
+// Chamada do link da conexão
+mysqli_query($link, "INSERT INTO tb_post2(tema,imagem,titulo,relevantes,noticia,dt,hr,usuario)VALUES('$tema','$imagem','$titulo','$relevantes','$noticia','$dt','$hr','$usuario')");
+
+header('location:/projetos-php/news/pages/form-post2.php');
+
+?>
