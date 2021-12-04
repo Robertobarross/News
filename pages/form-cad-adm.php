a261986d0548413d8d20e63b36446b82cbc05d9b
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-12-03 23:44:06 -0300

    Update form-cad-adm.php

--- pages/form-cad-adm.php
+++ pages/form-cad-adm.php
@@ -1,3 +1,26 @@
+<?php // Restrição a usuário não logado
+
+session_start(); // Estartando a sessão
+
+if(isset($_POST['email'])) { // Declarando variáveis da conexão
+    $email = $_POST['email'];
+}
+
+if(isset($_POST['password'])) { // Declarando variáveis da conexão
+    $password = $_POST['password'];
+
+    if($email == "email" and $password == "password") { // Se email e password estiveram cadastrados
+        $_SESSION['login'] =  true; // O login será autorizado
+    }
+}
+
+if(!isset($_SESSION['login'])) { // Se o usuário não tiver cadastro será redirecionado para tela de login
+    header('location:login.php');
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
