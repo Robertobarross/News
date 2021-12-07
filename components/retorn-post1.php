8ad861104de199343aba0eb03057cad4e663a0cb
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-12-06 23:32:46 -0300

    Update retorn-post1.php

--- components/retorn-post1.php
+++ components/retorn-post1.php
@@ -22,14 +22,14 @@ $sql = mysqli_query($link, "SELECT * FROM tb_post1 ORDER BY tema DESC");// faz a
 <div id="post1">
 
     <!--Campos da postagem principal-->
-    <p>Tema: <?php echo $tema;?></p>
-    <p>Notícia: <?php echo $titulo;?></p>
+    <p class="campos-post1">Tema: <?php echo $tema;?></p>
+    <p class="campos-post1">Notícia: <?php echo $titulo;?></p>
     <br><img src="imgs-posts/<?php echo ""."/".$imagem;?>" width="100%"> 
-    <p><?php echo $relevantes;?></p>
-    <p><?php echo $noticia;?></p>
-    <p>Data: <?php echo $dt;?></p>
-    <p>Hora: <?php echo $hr;?></p>
-    <p>Reporter: <?php echo $usuario;?></p>
+    <p class="campos-post1"><?php echo $relevantes;?></p>
+    <p class="campos-post1"><?php echo $noticia;?></p>
+    <p class="campos-post1">Data: <?php echo $dt;?></p>
+    <p class="campos-post1">Hora: <?php echo $hr;?></p>
+    <p class="campos-post1">Reporter: <?php echo $usuario;?></p>
 
 </div>
 <!--Fim da div da postagem-->
@@ -44,12 +44,13 @@ $sql = mysqli_query($link, "SELECT * FROM tb_post1 ORDER BY tema DESC");// faz a
 
     #post1{
         width: 40%;
-        margin-left: 30px;
+        margin-left: 90px;
         margin-top: 80px;
+        float: left;
     }
 
-    p{
-        font-size: 15px;
+    .campos-post1{
+        font-size: 13px;
         font-family: Arial;
         color: #363636;
         text-align: justify;
